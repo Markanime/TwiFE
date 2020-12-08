75b6c67fc5cf0837224b20d956ed1f2b8abd21c9
Marco Antonio Palacios <mpalacio@egogames.com>
2020-12-09 00:51:54 +0100

    BasicMessageBox

--- Assets/Scripts/TwifeMessageBox.cs
+++ Assets/Scripts/TwifeMessageBox.cs
@@ -9,7 +9,14 @@ public class TwifeMessageBox : MonoBehaviour
 
     public Image channelIcon;
     public TypingAnimation text;
-    public List<ChatterWithImages> queue = new List<ChatterWithImages>();
+    public Text userName;
+    private List<ChatterWithImages> queue = new List<ChatterWithImages>();
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     public void Push(ChatterWithImages chatter)
     {
         queue.Add(chatter);
@@ -30,12 +37,30 @@ public class TwifeMessageBox : MonoBehaviour
         else
         {
             text.timer = timerRange.y;
+            if (text.idleSeconds > maxSeconds * 3)
+            {
+                animator.SetBool("hide",true);
+            }
         }
     }
 
     private void Print(ChatterWithImages chatter)
     {
-        StartCoroutine(chatter.channel.GetTexture(channelIcon));
+        if(chatter.channel.id != userName.text)
+            animator.SetBool("hide", true);
+        else
+            animator.SetBool("hide", false);
+        StartCoroutine(Chat(chatter));
+
+    }
+
+    IEnumerator Chat(ChatterWithImages chatter)
+    {
+        yield return chatter.channel.GetTexture(channelIcon);
+        userName.text = chatter.channel.id;
+        animator.SetBool("hide", false);
+        if(!animator.GetCurrentAnimatorStateInfo(0).IsName("show"))
+            animator.SetTrigger("show");
         text.Type(chatter);
     }
 }
